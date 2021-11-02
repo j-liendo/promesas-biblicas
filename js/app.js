1b5b08a4bc37340ae34659640d25493aff75c1cb
josh1502-bit <72353245+josh1502-bit@users.noreply.github.com>
2021-11-02 10:37:20 -0400

    -

--- js/app.js
+++ js/app.js
@@ -331,7 +331,7 @@ const photosUrl = () => {
 $(document).ready(function() {
     console.log("conncet");
 
-    // getRandomVerse()
+    getRandomVerse()
 
     $("#new-verse").on("click", () => {
         hideAlert();
